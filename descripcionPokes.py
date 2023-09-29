4a03e157c0a4e7c194b7bc05c427027080977531
pavelio1303 <pablojimenez910.alu@stemgranada.com>
2023-09-29 09:05:34 +0200

    Definimos Pokemon

---
+++ descripcionPokes.py
@@ -0,0 +1,5 @@
+print("1. Este Pokémon es una serpiente venenosa con un patrón de colores brillantes en su cuerpo. Puede aprender movimientos de tipo veneno y tipo planta")
+print("2. Es un Pokémon con una concha dura y puede disparar agua a alta presión desde sus cañones de agua en su espalda. Es de tipo agua")
+print("3. Este Pokémon es un pájaro grande de tipo volador. Es conocido por su velocidad y su capacidad para crear ráfagas de viento con sus alas.")
+print("4. Es un Pokémon dragón que puede lanzar llamas ardientes de su boca. Es el último eslabón de una evolución de tres etapas.")
+print("5. Este Pokémon es un pequeño roedor eléctrico con mejillas rojas y amarillas. Es la mascota más icónica de la franquicia Pokémon.")
